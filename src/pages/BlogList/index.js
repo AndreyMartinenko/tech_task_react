46f6b23499d2d85650b248c8610ced4443a38964
AndreyMartinenko <Andremartinenko@gmail.com>
2018-07-25 03:27:52 +0300

    fixed err. comented import

--- src/pages/BlogList/index.js
+++ src/pages/BlogList/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+ import React, {Component} from 'react'
 
 import './styles.css'
 import {Link} from 'react-router-dom'
--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
 import rootReducer from './redusers/rootReducer.js'
-import { composeWithDevTools } from 'redux-devtools-extension'
+// import { composeWithDevTools } from 'redux-devtools-extension'
 
 export const history = createHistory()
 
